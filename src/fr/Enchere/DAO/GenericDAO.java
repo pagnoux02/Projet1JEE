3cfe40616bd1c8618863a831981dae8b68256865
adrien <adrien26poiroux@gmail.com>
2020-04-15 17:46:31 +0200

    le commit du trool
    Lol

--- src/fr/Enchere/DAO/GenericDAO.java
+++ src/fr/Enchere/DAO/GenericDAO.java
@@ -14,11 +14,7 @@ public interface GenericDAO<T> {
 	public String insert(T t) throws DAOException,FunctionnalException;
 	
 	public String update(T t) throws DAOException,FunctionnalException;
-	
-<<<<<<< HEAD
-	public String Delete(int id) throws DAOException,FunctionnalException;
-=======
+
 	public String delete(int t) throws DAOException,FunctionnalException;
->>>>>>> master
 
 }
