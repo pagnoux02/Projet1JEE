8eae50bef69c20a3060a4996b653b3fe900cc6e2
adrien <adrien26poiroux@gmail.com>
2020-04-15 11:47:58 +0200

    Commit utilisateur
    Generic DAO
    Utiisateur BO
    les exception

---
+++ src/fr/Enchere/Exception/DAOException.java
@@ -0,0 +1,14 @@
+package fr.Enchere.Exception;
+
+public class DAOException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DAOException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
