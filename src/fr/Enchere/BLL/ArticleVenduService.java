86b6435f91aaea5d02fdaff2bdfe7f6b004852e0
ilan <ilan.glet.56@gmail.com>
2020-04-17 14:38:40 +0200

    article vendu service debut

---
+++ src/fr/Enchere/BLL/ArticleVenduService.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package fr.Enchere.BLL;
+
+import java.util.List;
+
+import fr.Enchere.BO.ArticleVendu;
+import fr.Enchere.Exception.BllException;
+
+/**
+ * @author ilang
+ *
+ */
+public class ArticleVenduService {
+	
+	public List<ArticleVendu> getAllArticleVendu() throws BllException{
+		
+		
+		
+		return null;
+	}
+}
